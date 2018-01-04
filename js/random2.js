bc59e8ac87bb286c5968bec551dba1c2df40b4d6
Xenia <xenia@Xenias-iMac.local>
2018-01-04 20:16:25 +0100

    pushing current site

---
+++ js/random2.js
@@ -0,0 +1,30 @@
+var photo = $('.grid img');
+photo.each(function(i,e){
+  $(this).css({
+    'max-width' : (29 + (Math.random() * 31) + 'vw')
+  });
+});
+
+var photoWrapper = $('.grid .photo');
+photoWrapper.each(function(i,e){
+  randomNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    if((i + 1) % randomNumber == 0){
+       $(e).addClass('random-left');
+    }
+});
+
+var photoWrapper = $('.grid .photo');
+photoWrapper.each(function(i,e){
+  randomNumber = Math.floor(Math.random() * (5 - 3 + 1)) + 3;
+    if((i + 1) % randomNumber == 0){
+       $(e).addClass('float-left');
+    }
+});
+
+var photoWrapper = $('.grid .photo');
+photoWrapper.each(function(i,e){
+  randomNumber = Math.floor(Math.random() * (8 - 3 + 1)) + 3;
+    if((i + 1) % randomNumber == 0){
+       $(e).addClass('random-top');
+    }
+});
